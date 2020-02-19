2fde042c456e19ae4fe2e462a5239142378e6f80
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-02-18 17:23:39 -0800

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'font-awesome/css/font-awesome.min.css';
+import './styles/index.css';
+import './styles/modal.css';
+import App from './components/App.js';
+import * as serviceWorker from './serviceWorker';
