3f6539b9a9d6e292e62f09c4f7050b3ff29f1f71
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-02-19 22:47:40 -0800

    added about and side menu

--- src/components/App.js
+++ src/components/App.js
@@ -61,7 +61,55 @@ class App extends React.Component{
         event.stopPropagation();
     }
 
-    //put about here
+    toggleAbout = () => {
+        this.setState(prevState => ({showAbout: !prevState.showAbout}));
+    }
+    
+    renderAbout = () => {
+        return (
+          <div className="modal" role="dialog">
+            <div className="modal-dialog modal-lg">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h3 className="modal-title"><b>about</b>
+                    <button className="close-modal-button" onClick={this.toggleAbout}>
+                      &times;</button>
+                  </h3>
+                </div>
+                <div className="modal-body">
+                  <img
+                  src="https://dl.dropboxusercontent.com/s/awuwr1vpuw1lkyl/SpeedScore4SplashLogo.png"
+                  height="200" width="200"/>
+                  <h3>this is IA5 by marco ares</h3>
+                  <p>version cpts489 spring 2020<br/>
+                  &copy; 2020 student at washington state university. all rights
+                  reserved.
+                  </p>
+                  <div style={{textAlign: "left"}}>
+                    <p>IA5 apps support</p>
+                    <p>this assignment is made by react that we learned in class
+                        in the past week and a half. mostly the code that i have 
+                        here is helped by going to class and the code from class.
+                        without that i would be lost in what i am doing because 
+                        react is a hard language to learn in a short period of time,
+                        but hey.. we are here now and im learning this bs and hopefully
+                        i get a good grade for this. i am honestly super tired. 
+                        but it is what is.
+                    </p>
+                    <p>follow me in instagram to support me<a
+                    href="https://www.instagram.com/jmarcoares/" target="_blank">@jmarcoares</a>
+                    </p>
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <button className="btn btn-primary btn-color-theme"
+                    onClick={this.toggleAbout}>OK</button>
+                  </div>
+              </div>
+            </div>
+          </div>
+        );
+    }
 
     render() {
         const ModePage = modeToPage[this.state.mode];
